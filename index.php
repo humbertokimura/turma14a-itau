ad9081d0075c1f257a133ecd0e5aa27c2a7745c4
humbertokimura <humberto.kimura@outlook.com>
2021-02-04 15:52:41 -0300

    agora com botoes

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
-<h1>Bem vindo turma 14A do itaú pele treinamento da Gama </h1>
-
-<h3> Um deploy aotomático feito com alunos do itaú </h3>
-
-<h5>Tentar sempre, desistir jamais!!!<h5>
-     
-  <h5> Sem desistir do conflito <h5>
-
-
 <div style="display:flex">
   <div style="width:200px;height:200px;background:yellow;padding:50px; margin:10px">
   Um teste em um quadrado
   <br>
   <img src="Salvia.jpg" style="width: 200px">
+  
+  <br><br>
+     R$ 550,00
+    <br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" >Entrar em contato com o vendedor</a>
 
   </div>
 
@@ -20,6 +17,12 @@
   Um teste em um quadrado
   <br>
   <img src="Boldo.jpg" style="width: 200px">
-  </div>
+
+<br><br>
+    R$ 850,00
+    <br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" style="color:white">Entrar em contato com o vendedor</a>
 
 
+</div>
