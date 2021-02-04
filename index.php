f65c50fdb209a4bc20f1eb85708d4ad2049b5245
humbertokimura <humberto.kimura@outlook.com>
2021-02-04 15:01:14 -0300

    Estilos no HTML index

--- index.php
+++ index.php
@@ -6,9 +6,17 @@
      
   <h5> Sem desistir do conflito <h5>
 
-  
-  <div style="width:200px;height:200px;background:yellow">
+
+<div style="display:flex">
+  <div style="width:200px;height:200px;background:yellow;padding:50px; margin:10px">
   Um teste em um quadrado
   </div>
 
+
+
   
+  <div style="width:200px;height:200px;background:blue;color:white;padding:50px; margin:10px">
+  Um teste em um quadrado
+  </div>
+
+
